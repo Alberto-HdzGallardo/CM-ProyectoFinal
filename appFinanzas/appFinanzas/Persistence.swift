bd5b1abb7b2e4b7381d9e802037595a1c942302e
Alberto J. Hdz. Gallardo <54250168+Alberto-HdzGallardo@users.noreply.github.com>
2025-05-28 14:08:04 -0600

    Update Persistence.swift

--- appFinanzas/appFinanzas/Persistence.swift
+++ appFinanzas/appFinanzas/Persistence.swift
@@ -9,25 +9,19 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-    @MainActor
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
+    
+    static var preview: PersistenceController = {
+        let controller = PersistenceController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        do{
             try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        } catch{
+            fatalError("Error creando datos de prueba para preview: \(error)")
         }
-        return result
+        return controller
     }()
+    
 
     let container: NSPersistentContainer
 
@@ -55,3 +49,6 @@ struct PersistenceController {
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
+
+
+
